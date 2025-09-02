16865f951b0d8d0f57ef88fb762ef7d146ec6d8e
Liliane Conteville <70072309+lconteville@users.noreply.github.com>
2025-09-02 09:45:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("form");
+const resultado = document.getElementById("resultado");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const letras = document.getElementById("inputLetras").value.toUpperCase();
+  const cancerCID = document.getElementById("cancertype").value;
+
+  if (letras.length !== 6 || !cancerCID) {
+    resultado.textContent = "Preencha a sigla e selecione um tipo de câncer.";
+    return;
+  }
+
+  // Regra para gerar o código
+  const codigo = `${letras}-${cancerCID}`;
+
+  resultado.textContent = "Código gerado: " + codigo;
+});
